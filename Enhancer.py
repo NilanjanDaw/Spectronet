b8b71aaa9f742caa87fcd0e7fbda6b47472c133c
atul04 <atulsahay01@gmail.com>
2018-11-22 16:23:08 +0530

    Enhancer: gamma_correction, sharpness

--- Enhancer.py
+++ Enhancer.py
@@ -2,29 +2,37 @@
 # @Date:   2018-11-22T14:22:47+05:30
 # @Email:  atulsahay01@gmail.com
 # @Last modified by:   atul
-# @Last modified time: 2018-11-22T15:20:17+05:30
+# @Last modified time: 2018-11-22T16:22:23+05:30
 
 import cv2
 import numpy as np
+import PIL as p
 
-# def butter_lowpass(cutoff, fs, order=5):
-#     nyq = 0.5 * fs
-#     normal_cutoff = cutoff / nyq
-#     b, a = signal.butter(order, normal_cutoff, btype='low', analog=False)
-#     return b, a
-#
-# def butter_lowpass_filter(data, cutoff, fs, order=5):
-#     b, a = butter_lowpass(cutoff, fs, order=order)
-#     y = signal.lfilter(b, a, data)
-#     return y
+def butter_lowpass(cutoff, fs, order=5):
+     nyq = 0.5 * fs
+     normal_cutoff = cutoff / nyq
+     b, a = signal.butter(order, normal_cutoff, btype='low', analog=False)
+     return b, a
 
-ImageName = 'a0036.png'
-sensitivity = 0.4
+def butter_lowpass_filter(data, cutoff, fs, order=5):
+     b, a = butter_lowpass(cutoff, fs, order=order)
+     y = signal.lfilter(b, a, data)
+     return y
+
+def sharpness(imageName,OUTPUT):
+    image = p.IMage.open(ImageName)
+    enhancer = p.ImageEnhance.Sharpness(image)
+    factor = 2 # how much shaprness we needed
+    ## TODO:  Will make it adaptive
+    enhancedImage = enhancer.enhance(factor)
+    enhancedImage.save(OUPUT)
 
 
-image_v = cv2.imread(ImageName)
-image_v = image_v/255.0
-gamma_corrected_image = cv2.pow(image_v,sensitivity)
-cv2.imshow('Original Image',image_v)
-cv2.imshow('Power Law Transformation',gamma_corrected_image)
-cv2.waitKey(0)
+def gamma_correction(imageName,OUTPUT,sensitivity=0.4):
+    image_v = cv2.imread(ImageName)
+    image_v = image_v/255.0
+    gamma_corrected_image = cv2.pow(image_v,sensitivity)
+    # cv2.imshow('Original Image',image_v)
+    # cv2.imshow('Power Law Transformation',gamma_corrected_image)
+    # cv2.waitKey(0)
+    cv2.imwrite(OUTPUT,gamma_corrected_image)
